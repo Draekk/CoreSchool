be461441151823793ed0a64392099c957631be9e
Draekk <rodriver1992@gmail.com>
2023-02-23 15:52:39 -0300

    Inicio ConsoleApp School

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+CoreSchool school = new CoreSchool();
+school.name = "Platzi Academy";
+school.address = "En internet";
+school.fYear = 2012;
+Console.WriteLine("Timbre");
+school.timbrar();
+
+class CoreSchool
+{
+    public string name {get; set;}
+    public string address {get; set;}
+    public int fYear {get; set;}
+    public string ceo {get; set;}
+
+    public void timbrar(){
+        //Todo
+        Console.Beep(10000, 3000);
+    }
+}
